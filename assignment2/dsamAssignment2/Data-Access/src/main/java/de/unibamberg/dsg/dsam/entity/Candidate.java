7ee41539b9c0ef806e2c507bf51318e557a9b89a
Kowshik Dipta Das Joy <kowshik-dipta-das.joy@stud.uni-bamberg.de>
2020-01-21 21:08:01 +0100

    again changed location

---
+++ assignment2/dsamAssignment2/Data-Access/src/main/java/de/unibamberg/dsg/dsam/entity/Candidate.java
@@ -0,0 +1,91 @@
+package de.unibamberg.dsg.dsam.entity;
+
+
+/**
+ * 
+ * @author Kowshik Dipta Das Joy
+ */
+public class Candidate {
+
+	private String ID;
+	private int CandidateID;
+	private String Faculty;
+	private String FirstName;
+	private String MiddleName;
+	private String SurName;
+	/**
+	 * @return the iD
+	 */
+	public String getID() {
+		return ID;
+	}
+	/**
+	 * @param iD the iD to set
+	 */
+	public void setID(String iD) {
+		ID = iD;
+	}
+	/**
+	 * @return the candidateID
+	 */
+	public int getCandidateID() {
+		return CandidateID;
+	}
+	/**
+	 * @param candidateID the candidateID to set
+	 */
+	public void setCandidateID(int candidateID) {
+		CandidateID = candidateID;
+	}
+	/**
+	 * @return the faculty
+	 */
+	public String getFaculty() {
+		return Faculty;
+	}
+	/**
+	 * @param faculty the faculty to set
+	 */
+	public void setFaculty(String faculty) {
+		Faculty = faculty;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return FirstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	/**
+	 * @return the middleName
+	 */
+	public String getMiddleName() {
+		return MiddleName;
+	}
+	/**
+	 * @param middleName the middleName to set
+	 */
+	public void setMiddleName(String middleName) {
+		MiddleName = middleName;
+	}
+	/**
+	 * @return the surName
+	 */
+	public String getSurName() {
+		return SurName;
+	}
+	/**
+	 * @param surName the surName to set
+	 */
+	public void setSurName(String surName) {
+		SurName = surName;
+	}
+	
+	
+	
+}
